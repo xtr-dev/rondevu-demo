73f04bc078d70b5a8ec77953dd1e0189b048dfb3
Bas van den Aakster <bvdaakster@gmail.com>
2025-11-14 21:07:37 +0100

    Restore Google STUN servers only (metered.ca DNS lookup failing)

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,9 @@ const API_URL = 'https://api.ronde.vu';
 
 const RTC_CONFIG = {
   iceServers: [
-    {
-      urls: "stun:stun.relay.metered.ca:80",
-    },
-    {
-      urls: "turn:standard.relay.metered.ca:80",
-      username: "e03a51621b4f11ffbed3addd",
-      credential: "QPjJzPau1Ng5S0dq",
-    },
-    {
-      urls: "turn:standard.relay.metered.ca:80?transport=tcp",
-      username: "e03a51621b4f11ffbed3addd",
-      credential: "QPjJzPau1Ng5S0dq",
-    },
-    {
-      urls: "turn:standard.relay.metered.ca:443",
-      username: "e03a51621b4f11ffbed3addd",
-      credential: "QPjJzPau1Ng5S0dq",
-    },
-    {
-      urls: "turns:standard.relay.metered.ca:443?transport=tcp",
-      username: "e03a51621b4f11ffbed3addd",
-      credential: "QPjJzPau1Ng5S0dq",
-    },
+    // Google STUN servers - reliable and always available
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
   ],
 };
 
